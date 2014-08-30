dce90d8272281e7f7f6e4a3023c89b9394225c6c
cristianames <cristianames.92@gmail.com>
2014-08-30 19:11:57 -0300

    Primeros pasos TGC

---
+++ Dibujable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX;
+using TgcViewer;
+using TgcViewer.Utils.TgcSceneLoader;
+using System.Drawing;
+
+
+namespace AlumnoEjemplos.TheGRID
+{
+    public struct Doble
+    {
+        private float actual;
+        private float anterior;
+        public void setActual(float nuevo)
+        {
+            anterior = actual;
+            actual = nuevo;
+        }
+        public float getActual() { return actual; }
+        public float getAnterior() { return anterior; }
+    }
+    //------------------------------------------------------------------
+    public struct Vector3Doble
+    {
+        private Vector3 actual;
+        private Vector3 anterior;
+        public void setActual(Vector3 nuevo){
+            anterior = actual;
+            actual = nuevo;
+        }
+        public void setActual(float X, float Y, float Z)
+        {
+            anterior = actual;
+            actual.X = X;
+            actual.Y = Y;
+            actual.Z = Z;
+        }
+        public Vector3 getActual() { return actual; }
+        public Vector3 getAnterior() { return anterior; }
+        public Vector3 direccion()
+        {
+            Vector3 temp = new Vector3(actual.X, actual.Y, actual.Z);
+            temp.Subtract(anterior);
+            return temp;
+        }
+    }
+    //------------------------------------------------------------------
+    public interface IComun : IRenderObject, ITransformObject
+    {
+    }
+    //------------------------------------------------------------------
+    class Dibujable
+    {
+        private Vector3Doble posicion;
+        private Doble aceleracion;
+        private Doble velocidad;
+        public Object objeto;
+        
+        public Dibujable()
+        {
+            posicion.setActual(0, 0, 0);
+            aceleracion.setActual(0);
+            velocidad.setActual(0);
+        }   
+    }
+}
---
+++ EjemploAlumno.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TgcViewer.Example;
+using TgcViewer;
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcGeometry;
+using AlumnoEjemplos.TheGRID;
+using TgcViewer.Utils.TgcSceneLoader;
+
+namespace AlumnoEjemplos.MiGrupo
+{
+    public class EjemploAlumno : TgcExample
+    {
+        /// Categoría a la que pertenece el ejemplo.
+        /// Influye en donde se va a ver en el árbol de la derecha de la pantalla.
+        public override string getCategory(){ return "AlumnoEjemplos"; }
+        /// Completar nombre del grupo en formato Grupo NN
+        public override string getName(){ return "Grupo TheGRID"; }
+        /// Completar con la descripción del TP
+        public override string getDescription(){return "Viaje Interplanetario - Misma idea";}
+        //--------------------------------------------------------
+        // ATRIBUTOS
+        TgcBox suelo;
+        Dibujable caja;
+        Dibujable asteroide;
+        //--------------------------------------------------------
+        public override void init()
+        {
+            //GuiController.Instance: acceso principal a todas las herramientas del Framework
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+            //Carpeta de archivos Media del alumno
+            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
+
+            {
+                ///////////////USER VARS//////////////////
+                //Crear una UserVar
+                GuiController.Instance.UserVars.addVar("variablePrueba");
+                //Cargar valor en UserVar
+                GuiController.Instance.UserVars.setValue("variablePrueba", 5451);
+
+                ///////////////MODIFIERS//////////////////
+                //Crear un modifier para un valor FLOAT
+                GuiController.Instance.Modifiers.addFloat("valorFloat", -50f, 200f, 0f);
+                //Crear un modifier para un ComboBox con opciones
+                string[] opciones = new string[] { "opcion1", "opcion2", "opcion3" };
+                GuiController.Instance.Modifiers.addInterval("valorIntervalo", opciones, 0);
+                //Crear un modifier para modificar un vértice
+                GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
+
+                ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
+                //Es la camara que viene por default, asi que no hace falta hacerlo siempre
+                GuiController.Instance.RotCamera.Enable = true;
+                //Configurar centro al que se mira y distancia desde la que se mira
+                GuiController.Instance.RotCamera.setCamera(new Vector3(0, 0, 0), 100);
+
+                /*
+                ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
+                //Camara en primera persona, tipo videojuego FPS
+                //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
+                //Por default la camara FPS viene desactivada
+                GuiController.Instance.FpsCamera.Enable = true;
+                //Configurar posicion y hacia donde se mira
+                GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
+                */
+
+                ///////////////LISTAS EN C#//////////////////
+                //crear
+                List<string> lista = new List<string>();
+                //agregar elementos
+                lista.Add("elemento1");
+                lista.Add("elemento2");
+                //obtener elementos
+                string elemento1 = lista[0];
+                //bucle foreach
+                foreach (string elemento in lista)
+                {
+                    //Loggear por consola del Framework
+                    GuiController.Instance.Logger.log(elemento);
+                }
+                //bucle for
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    string element = lista[i];
+                }
+            }
+           /*
+            //Creamos una caja 3D de color rojo, ubicada en el origen y lado 10
+            Vector3 center = new Vector3(0, 0, 0);
+            Vector3 size = new Vector3(10, 10, 10);
+            Color color = Color.Red;
+            caja = new Dibujable();
+            caja.objeto = TgcBox.fromSize(center, size, color);
+            TgcBox cajita = (TgcBox)caja.objeto;
+            //GuiController.Instance.RotCamera.targetObject(cajita.BoundingBox);
+            */
+            //Crear suelo
+            TgcTexture pisoTexture = TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesMediaDir + "Texturas\\Quake\\TexturePack2\\rock_floor1.jpg");
+            suelo = TgcBox.fromSize(new Vector3(500, 0, 500), new Vector3(2000, 0, 2000), pisoTexture);
+            //
+            asteroide = new Dibujable();
+            TgcSceneLoader loader = new TgcSceneLoader();
+            TgcScene scene = loader.loadSceneFromFile(GuiController.Instance.AlumnoEjemplosMediaDir + "Asteroide\\esferita-TgcScene.xml");
+            asteroide.objeto = scene.Meshes[0];
+            
+            //GuiController.Instance.RotCamera.targetObject(asteroide.BoundingBox);
+
+        }
+
+
+        /// <summary>
+        /// Método que se llama cada vez que hay que refrescar la pantalla.
+        /// Escribir aquí todo el código referido al renderizado.
+        /// Borrar todo lo que no haga falta
+        /// </summary>
+        /// <param name="elapsedTime">Tiempo en segundos transcurridos desde el último frame</param>
+        public override void render(float elapsedTime)
+        {
+            //Device de DirectX para renderizar
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+
+
+            //Obtener valor de UserVar (hay que castear)
+            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
+
+
+            //Obtener valores de Modifiers
+            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
+            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
+            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
+
+
+            ///////////////INPUT//////////////////
+            //conviene deshabilitar ambas camaras para que no haya interferencia
+
+            //Capturar Input teclado 
+            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            {
+                //Tecla F apretada
+            }
+
+            //Capturar Input Mouse
+            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            {
+                //Boton izq apretado
+            }
+            
+            ((TgcMesh)asteroide.objeto).render();
+            suelo.render();
+
+        }
+
+        /// <summary>
+        /// Método que se llama cuando termina la ejecución del ejemplo.
+        /// Hacer dispose() de todos los objetos creados.
+        /// </summary>
+        public override void close()
+        {
+
+        }
+
+    }
+}
