9d0bfa15fdef96ac051fc3904ae6a68038c8c798
tferraro <tetferraro@gmail.com>
2014-09-07 00:21:36 -0300

    Cambio de la camara

--- Camara/CambioCamara.cs
+++ Camara/CambioCamara.cs
@@ -19,51 +19,43 @@ namespace AlumnoEjemplos.TheGRID.Camara
         public CambioCamara(Dibujable dibujable)
         {
             objeto_foco = dibujable;
-            switch (modo)
-            {
-                case TipoModo.FPS:
-                    habilitarFirst();
-                    break;
-                case TipoModo.TPS:
-                    habilitarThird();
-                    break;
-                case TipoModo.Exterior:
-                    habilitarExterior();
-                    break;
-            }
+            cambiarPosicionCamara();
+
         }
         public void modoFPS()
         {
-            GuiController.Instance.RotCamera.Enable = false;
-            habilitarFirst();
+            modo = TipoModo.FPS;
+            moverFirst();
         }
         public void modoTPS()
         {
-            GuiController.Instance.RotCamera.Enable = false;
-            habilitarThird();
+            modo = TipoModo.TPS;
+            moverThird();
         }
         public void modoExterior()
         {
-            GuiController.Instance.ThirdPersonCamera.Enable = false;
             habilitarExterior();
         }
-        private void habilitarThird()
+        private void moverThird()
         {
-            modo = TipoModo.TPS;
-            GuiController.Instance.ThirdPersonCamera.Enable = true;
-            GuiController.Instance.ThirdPersonCamera.setCamera(objeto_foco.getCentro(), 200, -300);
+            Vector3 posicionDeCamara = objeto_foco.getPosicion();
+            Vector3 temp = objeto_foco.getDireccion();
+            temp.Multiply(100);
+            posicionDeCamara -= temp;
+            temp = new Vector3(0, 50, 0);
+            posicionDeCamara += temp;
+            temp = objeto_foco.getDireccion_Y();
+            temp.Multiply(15);
+            GuiController.Instance.setCamera(posicionDeCamara, objeto_foco.getPosicion() + temp);
         }
-        private void habilitarFirst()
-        {   
-            modo = TipoModo.FPS;
-            GuiController.Instance.ThirdPersonCamera.Enable = true;
-            GuiController.Instance.ThirdPersonCamera.setCamera(objeto_foco.getCentro(), 0, -0.1f);
+        private void moverFirst()
+        {
+            GuiController.Instance.setCamera(objeto_foco.getPosicion() - objeto_foco.getDireccion(), objeto_foco.getPosicion());
+
         }
         private void habilitarExterior()
         {
-            modo = TipoModo.Exterior;
-            GuiController.Instance.RotCamera.Enable = true;
-            GuiController.Instance.RotCamera.CameraCenter = objeto_foco.getCentro();
+
         }
         public void cambiarFoco(Dibujable dibujable){
             objeto_foco = dibujable;
@@ -72,28 +64,19 @@ namespace AlumnoEjemplos.TheGRID.Camara
 
         public void cambiarPosicionCamara()
         {
-            if (modo != TipoModo.Exterior)
+            switch (modo)
             {
-                GuiController.Instance.ThirdPersonCamera.Target = objeto_foco.getCentro();
-                if (objeto_foco.giro != 0)
-                {
-                    //float angulo = FastMath.Acos(dotProduct(objeto_foco.getDireccion(), objeto_foco.getDireccionAnterior()));
-                    float dot = dotProduct(objeto_foco.getDireccion(), objeto_foco.getDireccionAnterior());
-                    float cross = crossProduct(objeto_foco.getDireccion(), objeto_foco.getDireccionAnterior()).Length();
-                    float angulo = (float) Math.Atan2(cross, dot);
-                    if (objeto_foco.giro < 0) GuiController.Instance.ThirdPersonCamera.rotateY(-angulo);
-                    if (objeto_foco.giro > 0) GuiController.Instance.ThirdPersonCamera.rotateY(angulo);
-                }
+                case TipoModo.FPS:
+                    moverFirst();
+                    break;
+                case TipoModo.TPS:
+                    moverThird();
+                    break;
+                case TipoModo.Exterior:
+                    habilitarExterior();
+                    break;
             }
+            GuiController.Instance.CurrentCamera.updateCamera();
         }
-        private float dotProduct(Vector3 v1, Vector3 v2) { return ((v1.X * v2.X) + (v1.Y * v2.Y) + (v1.Z * v2.Z))/(v1.Length()*v2.Length()); }
-        private Vector3 crossProduct(Vector3 v1, Vector3 v2)
-        {
-            float x = v1.Y * v2.Z - v1.Z * v2.Y;
-            float y = v1.Z * v2.X - v1.X * v2.Z;
-            float z = v1.X * v2.Y - v1.Y * v2.X;
-            return new Vector3(x, y, z);
-        }
-        //No se divide por los modulos xq se utiliza para versores
     }
 }
